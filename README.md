ca04fc6c3f353c4e63709a9efab4e70d04acaaf3
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-06-13 12:40:30 +0800

    add loading status to disable buttons, add Radix Button detection

--- README.md
+++ README.md
@@ -1,22 +1,12 @@
-# create-svelte
+# Svelte-Tailwind-Skeleton-TypeScript
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+## Deployed At
+ https://svelte-tailwind-skeleton.vercel.app/
+## Installation
 
-## Creating a project
+`npm install` or `pnpm install` or `yarn`
 
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npm create svelte@latest
-
-# create a new project in my-app
-npm create svelte@latest my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+To start a development server:
 
 ```bash
 npm run dev
--- src/routes/radix/+page.svelte
+++ src/routes/radix/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import { toastStore, type ToastSettings } from '@skeletonlabs/skeleton';
+	import { toastStore, type ToastSettings, ProgressRadial } from '@skeletonlabs/skeleton';
 	import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit';
 	import {
 		buyGumball,
@@ -18,11 +18,12 @@
 	const dAppId = 'account_tdx_c_1pyu3svm9a63wlv6qyjuns98qjsnus0pzan68mjq2hatqejq9fr';
 
 	// Global states
-	let userAccountAddr = ''; // User account address
+	let radixStatus = '';
+	let accountName = '';
+	let userAccountAddr = '';
 	let packageAddress = '';
 	let gumballSymbol = '';
-	let accountName = 'none';
-	let staffBadge = 'none';
+	let staffBadge = '';
 	let withdrawnAmt = '';
 	let priceOutFromGetPrice = '';
 	let priceOutFromSetPrice = '';
@@ -35,6 +36,13 @@
 	let feePaid = '';
 	let t: ToastSettings;
 	let data: any;
+	let instantiateCompo_loading = false;
+	let getBalances_loading = false;
+	let buyGumball_loading = false;
+	let getPrice_loading = false;
+	let setPrice_loading = false;
+	let withdrawEarnings_loading = false;
+	let mintStaffBadge_loading = false;
 	//After deploying the package
 	let packageAddressOut = 'package_tdx_c_1qqy67sxv4e2nvls059cutjn5rk8edcvh3a4da7zusgusmkeahk';
 	let packageOwnerBadge = 'resource_tdx_c_1q27nneue67zpuxw6h96j3m86wdhxav8n8q6zymkpfuksxjlcz7';
@@ -75,13 +83,21 @@
 					if (accounts && accounts.length > 0) {
 						accountName = accounts[0].label;
 						userAccountAddr = accounts[0].address;
+						if (accountName && userAccountAddr) {
+							radixStatus = 'onInit successful with valid account';
+						}
 					} else {
 						lg('error... accounts:', accounts);
+            radixStatus = 'onInit failed. Please install RadixButton and click on the Connect button';
 					}
 				}
 			}
 		);
 		lg(' Radix dApp Toolkit(rdt): ', rdt);
+		if (rdt) {
+			radixStatus = 'initialized with unknown accounts';
+      lg('radixStatus: '+ radixStatus);
+		}
 
 		//gumballBalance = await getTokenBalance(userAccountAddr, tokenAddress);
 		//lg('gumballBalance:', gumballBalance);
@@ -90,22 +106,24 @@
 		return {
 			message: 'Transaction failed. Error message: ' + err,
 			background: 'variant-filled-error',
-			timeout: 2000
+			timeout: 5000
 		};
 	};
 	export const getToastOk = (): ToastSettings => {
 		return {
 			message: 'Transaction processed successfully!',
 			background: 'variant-filled-success',
-			timeout: 2000
+			timeout: 5000
 		};
 	};
 
 	const instantiateCompo = async () => {
 		lg('instantiateCompo()');
+		instantiateCompo_loading = true;
 		res = await instantiateComponent(packageAddress, userAccountAddr, 1);
+		instantiateCompo_loading = false;
 		if (res.error) {
-			t = getToastErr(res.error);
+			t = getToastErr(JSON.stringify(res.error));
 			toastStore.trigger(t);
 			return true;
 		}
@@ -117,16 +135,20 @@
 
 	const getBalances = async () => {
 		lg('getBalances()');
+		getBalances_loading = true;
 		gumballBalance = await getTokenBalance(userAccountAddr, tokenAddress);
 		xrdBalance = await getXrdBalance(userAccountAddr);
+		getBalances_loading = false;
 	};
 
 	//Write Functions
 	const buyGumballHandler = async () => {
 		lg('buyGumballHandler()');
+		buyGumball_loading = true;
 		res = await buyGumball(rdt, userAccountAddr, compoAddr);
+		buyGumball_loading = false;
 		if (res.error) {
-			t = getToastErr(res.error);
+			t = getToastErr(JSON.stringify(res.error));
 			toastStore.trigger(t);
 			return true;
 		}
@@ -140,9 +162,11 @@
 
 	const getPriceHandler = async () => {
 		lg('getPriceHandler()');
+		getPrice_loading = true;
 		res = await getPrice(rdt, compoAddr);
+		getPrice_loading = false;
 		if (res.error) {
-			t = getToastErr(res.error);
+			t = getToastErr(JSON.stringify(res.error));
 			toastStore.trigger(t);
 			return true;
 		}
@@ -155,9 +179,11 @@
 
 	const setPriceHandler = async () => {
 		lg('setPriceHandler()');
+		setPrice_loading = true;
 		res = await setPrice(rdt, userAccountAddr, compoAddr, admin_badge, setPriceInput);
+		setPrice_loading = false;
 		if (res.error) {
-			t = getToastErr(res.error);
+			t = getToastErr(JSON.stringify(res.error));
 			toastStore.trigger(t);
 			return true;
 		}
@@ -168,12 +194,13 @@
 		lg('priceOutFromSetPrice:', priceOutFromSetPrice);
 	}; //setPriceHandler
 
-
 	const withdrawEarningsHandler = async () => {
 		lg('withdrawEarningsHandler()');
+		withdrawEarnings_loading = true;
 		res = await withdrawEarnings(rdt, userAccountAddr, compoAddr, admin_badge);
+		withdrawEarnings_loading = false;
 		if (res.error) {
-			t = getToastErr(res.error);
+			t = getToastErr(JSON.stringify(res.error));
 			toastStore.trigger(t);
 			return true;
 		}
@@ -186,9 +213,11 @@
 
 	const mintStaffBadgeHandler = async () => {
 		lg('mintStaffBadgeHandler()');
+		mintStaffBadge_loading = true;
 		res = await mintStaffBadge(rdt, userAccountAddr, compoAddr, admin_badge);
+		mintStaffBadge_loading = false;
 		if (res.error) {
-			t = getToastErr(res.error);
+			t = getToastErr(JSON.stringify(res.error));
 			toastStore.trigger(t);
 			return true;
 		}
@@ -211,12 +240,16 @@
 	};
 </script>
 
+<!-- HTML Elements -->
 <div class="card p-4 w-full text-token space-y-4">
 	<h3 class="text-center py-2">Gumball Machine dApp</h3>
 
 	{#if isAfterLoadEvent}
-		<div class="connectBtn flex flex-row-reverse">
-			<radix-connect-button />
+		<div class="flex flex-row justify-between">
+			<div class="connectBtn">
+				<radix-connect-button />
+			</div>
+			<div class="mt-2">Radix Network: {radixStatus}</div>
 		</div>
 	{/if}
 
@@ -229,9 +262,19 @@
 	<input type="text" placeholder="Package address" bind:value={packageAddress} />
 	<input type="text" placeholder="Gumball Symbol" bind:value={gumballSymbol} />
 	<p>
-		<button type="button" class="btn btn-sm variant-filled-primary" on:click={instantiateCompo}
-			>Instantiate component</button
-		>
+		{#if instantiateCompo_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-14"
+			/>
+		{:else}
+			<button type="button" class="btn btn-sm variant-filled-primary" on:click={instantiateCompo}
+				>Instantiate component</button
+			>
+		{/if}
 	</p>
 
 	<div class="container">
@@ -240,16 +283,41 @@
 		<p>admin_badge address: {admin_badge}</p>
 	</div>
 
-	<p>
-		<button type="button" class="btn btn-sm variant-filled-error" on:click={buyGumballHandler}
-			>Buy 1 GUM</button
-		>
+	<div class="flex flex-row">
+		{#if getBalances_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-14"
+			/>
+		{:else}
+			<button type="button" class="btn variant-filled-secondary" on:click={() => getBalances()}
+				>Get Balances</button
+			>
+		{/if}
+		<div class="mt-2">
+			Balance: {gumballBalance} Tokens, {xrdBalance} XRD
+		</div>
+	</div>
 
-		<button type="button" class="btn btn-sm variant-filled-secondary" on:click={() => getBalances()}
-			>Get Balances</button
-		>
-		Balance: {gumballBalance} Tokens, {xrdBalance} XRD
-	</p>
+	<div class="flex flex-row">
+		{#if buyGumball_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-16"
+			/>
+		{:else}
+			<button type="button" class="btn variant-filled-error" on:click={buyGumballHandler}
+				>Buy 1 Gum Token</button
+			>
+		{/if}
+	</div>
+
 	<div class="container">
 		<p>
 			Receipt:<br />
@@ -258,32 +326,82 @@
 		</pre>
 	</div>
 
-	<p>
-		<button type="button" class="btn btn-sm variant-filled-secondary" on:click={getPriceHandler}
-			>Get Price</button
-		>
-		Price: {priceOutFromGetPrice}
-	</p>
+	<div class="flex flex-row">
+		{#if getPrice_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-14"
+			/>
+		{:else}
+			<button type="button" class="btn variant-filled-secondary" on:click={getPriceHandler}
+				>Get Price</button
+			>
+		{/if}
+		<div class="mt-2">
+			Price from GetPrice: {priceOutFromGetPrice}
+		</div>
+	</div>
 
-	<p>
+	<div class="flex flex-row">
 		<input type="text" placeholder="New Price" bind:value={setPriceInput} />
-		<button type="button" class="btn btn-sm variant-filled-error" on:click={setPriceHandler}
-			>Set Price</button
-		>
-		Price: {priceOutFromSetPrice}
-	</p>
+		{#if setPrice_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-14"
+			/>
+		{:else}
+			<button type="button" class="btn variant-filled-error" on:click={setPriceHandler}
+				>Set Price</button
+			>
+		{/if}
+		<div class="mt-2">
+			Price from SetPrice: {priceOutFromSetPrice}
+		</div>
+	</div>
 
-	<p>
-		<button type="button" class="btn btn-sm variant-filled-error" on:click={withdrawEarningsHandler}
-			>Withdraw Earnings</button
-		>
-		Withdrawn Amount: {withdrawnAmt}
-	</p>
+	<div class="flex flex-row">
+		{#if withdrawEarnings_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-14"
+			/>
+		{:else}
+			<button
+				type="button"
+				class="btn btn-sm variant-filled-error"
+				on:click={withdrawEarningsHandler}>Withdraw Earnings</button
+			>
+		{/if}
+		<div class="mt-1">
+			Withdrawn Amount: {withdrawnAmt}
+		</div>
+	</div>
 
-	<p>
-		<button type="button" class="btn btn-sm variant-filled-error" on:click={mintStaffBadgeHandler}
-			>Mint Staff Badge</button
-		>
-		Badge Minted: {staffBadge}
-	</p>
+	<div class="flex flex-row">
+		{#if mintStaffBadge_loading}
+			<ProgressRadial
+				...
+				stroke={220}
+				meter="stroke-primary-500"
+				track="stroke-primary-500/30"
+				width="w-14"
+			/>
+		{:else}
+			<button type="button" class="btn btn-sm variant-filled-error" on:click={mintStaffBadgeHandler}
+				>Mint Staff Badge</button
+			>
+		{/if}
+		<div class="mt-1">
+			Badge Minted: {staffBadge}
+		</div>
+	</div>
 </div>
