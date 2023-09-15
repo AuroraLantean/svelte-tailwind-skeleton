c2fd1afdf5aa06c8f47a5344618193ec56cf37a8
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-09-15 11:10:42 +0800

    update dependencies, disable Moralis

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,6 @@ import { PRIVATE_MORALIS } from "$env/static/private";
 
 console.log('inside hooks.server.ts ...');
 
-await Moralis.start({
-  apiKey: PRIVATE_MORALIS,
-});
+// await Moralis.start({
+//   apiKey: PRIVATE_MORALIS,
+// });
