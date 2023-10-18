9aecdd0f8796565e3fa08349609c40af66d04978
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-10-18 08:44:54 +0800

    upgrade packages, disable Moralis APIs

--- src/routes/ethereum/+page.svelte
+++ src/routes/ethereum/+page.svelte
@@ -2,21 +2,37 @@
 	import { onMount } from 'svelte';
 	import { toastStore, type ToastSettings, ProgressRadial } from '@skeletonlabs/skeleton';
 	import { PUBLIC_ADDR } from '$env/static/public';
-	import { ethersInit, getBalanceEth, erc20BalanceOf, erc20Transfer, erc20Approve, erc20Allowance, erc721Transfer, erc721BalanceOf } from '@store/ethers';
+	import {
+		ethersInit,
+		getBalanceEth,
+		erc20BalanceOf,
+		erc20Transfer,
+		erc20Approve,
+		erc20Allowance,
+		erc721Transfer,
+		erc721BalanceOf
+	} from '@store/ethers';
 	import { capitalizeFirst, getChainObj } from '@store/lib';
-  import goldcoin from "@contracts/goldcoin.json";
-  import dragonNft from "@contracts/erc721Dragon.json";
+	import goldcoin from '@contracts/goldcoin.json';
+	import dragonNft from '@contracts/erc721Dragon.json';
 
 	const lg = console.log;
-	lg('PUBLIC_ADDR:', PUBLIC_ADDR, ', goldcoin addr:', goldcoin.address, ', dragonNft addr:', dragonNft.address );
+	lg(
+		'PUBLIC_ADDR:',
+		PUBLIC_ADDR,
+		', goldcoin addr:',
+		goldcoin.address,
+		', dragonNft addr:',
+		dragonNft.address
+	);
 
-  let ethBalance = '';
-  let tokenBalance = '';
-  let nftBalance = '';
-  let txnHash_transfer20 = '';
-  let txnHash_transfer721 = '';
-  let txnHash_approve20 = '';
-  let tokenAllowance = '';
+	let ethBalance = '';
+	let tokenBalance = '';
+	let nftBalance = '';
+	let txnHash_transfer20 = '';
+	let txnHash_transfer721 = '';
+	let txnHash_approve20 = '';
+	let tokenAllowance = '';
 	let nativeBalance: any = '';
 	let tokenBalances: any = '';
 	let nftmetadata: any = '';
@@ -26,7 +42,7 @@
 	let receipt = '';
 	//------------------------==
 	export let pagetype = 'Ethereum Related Chains';
-  export let ctrtName = 'goldcoin';
+	export let ctrtName = 'goldcoin';
 	let res: any;
 	let t: ToastSettings;
 	let data: any;
@@ -65,63 +81,65 @@
 		lg('onMount');
 		isOnMount = true;
 		lg('isOnMount:', isOnMount);
-    chainDetected = capitalizeFirst('unknown');
+		chainDetected = capitalizeFirst('unknown');
 
-    const out = await ethersInit();
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		const out = await ethersInit();
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
 			return true;
 		}
-    if (out.warn) {
-      t = getToastWarn(JSON.stringify(out.warn));
+		if (out.warn) {
+			t = getToastWarn(JSON.stringify(out.warn));
 			toastStore.trigger(t);
 			return true;
 		}
 		t = getToastOk('web3 initialized successfully!');
 		toastStore.trigger(t);
 
-    addrDetected = out.account;
-    input.addr1 = addrDetected;
+		addrDetected = out.account;
+		input.addr1 = addrDetected;
 		lg('input.addr1: ', input.addr1, ', input.addr2: ', input.addr2);
-    const [chainObj, chainStr] = getChainObj(out.chainId);
-    lg('chainObj:', chainObj, ', chainStr:', chainStr);
-    chainDetected = capitalizeFirst(chainStr);
-  });
-  $: {
-    input.ctrtAddr = getCtrtAddr(ctrtName);
-  }
-  export const getCtrtAddr = (ctrtName: string): string => {
-    let ctrtAddr = '';
-    switch (ctrtName) {
-      case 'goldcoin':
-        ctrtAddr = goldcoin.address;
-        break;
-      case 'erc721Dragon':
-        ctrtAddr = dragonNft.address;
-        break;
-    }
-    lg('getCtrtAddr()... ctrtAddr', ctrtAddr);
-    return ctrtAddr;
+		const [chainObj, chainStr] = getChainObj(out.chainId);
+		lg('chainObj:', chainObj, ', chainStr:', chainStr);
+		chainDetected = capitalizeFirst(chainStr);
+	});
+	$: {
+		input.ctrtAddr = getCtrtAddr(ctrtName);
+	}
+	export const getCtrtAddr = (ctrtName: string): string => {
+		let ctrtAddr = '';
+		switch (ctrtName) {
+			case 'goldcoin':
+				ctrtAddr = goldcoin.address;
+				break;
+			case 'erc721Dragon':
+				ctrtAddr = dragonNft.address;
+				break;
+		}
+		lg('getCtrtAddr()... ctrtAddr', ctrtAddr);
+		return ctrtAddr;
 	};
 
-  export const getToastErr = (err: string): ToastSettings => {
+	export const getToastErr = (err: string): ToastSettings => {
 		return {
 			message: 'Transaction failed. Error message: ' + err,
 			background: 'variant-filled-error',
 			timeout: 5000
 		};
 	};
-	export const getToastOk = (mesg: string = 'Transaction processed successfully!'): ToastSettings => {
+	export const getToastOk = (
+		mesg: string = 'Transaction processed successfully!'
+	): ToastSettings => {
 		return {
-			message: 'Success. '+ mesg,
+			message: 'Success. ' + mesg,
 			background: 'variant-filled-success',
 			timeout: 5000
 		};
 	};
 	export const getToastWarn = (mesg: string = ''): ToastSettings => {
 		return {
-			message: 'Warn: '+ mesg,
+			message: 'Warn: ' + mesg,
 			background: 'variant-filled-warning',
 			timeout: 5000
 		};
@@ -152,35 +170,35 @@
 		lg('addr:', input.addr1, ', chainName:', input.chainName);
 		const res = await fetch(`/api/${input.chainName}/${input.addr1}/nftmetadata`);
 		const out = await res.json(); //array of xyz
-    lg('out:', out)
-    nftmetadata = JSON.stringify(out);
+		lg('out:', out);
+		nftmetadata = JSON.stringify(out);
 		lg('nftmetadata:', nftmetadata, typeof nftmetadata);
 		getNftmetadata_loading = false;
 	};
 
-  const hErc20Transfer = async (input: TxnInput) => {
+	const hErc20Transfer = async (input: TxnInput) => {
 		lg('hErc20Transfer() ... input:', input);
 		hErc20Transfer_loading = true;
 		const out = await erc20Transfer(input);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      hErc20Transfer_loading = false;
+			hErc20Transfer_loading = false;
 			return true;
 		}
 		t = getToastOk('');
 		toastStore.trigger(t);
-    txnHash_transfer20 = out.txnHash;
+		txnHash_transfer20 = out.txnHash;
 		hErc20Transfer_loading = false;
 	};
 	const hErc20Approve = async (input: TxnInput) => {
 		lg('hErc20Approve() ... input:', input);
 		hErc20Approve_loading = true;
 		const out = await erc20Approve(input);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      hErc20Approve_loading = false;
+			hErc20Approve_loading = false;
 			return true;
 		}
 		t = getToastOk('');
@@ -188,14 +206,14 @@
 		txnHash_approve20 = out.txnHash;
 		hErc20Approve_loading = false;
 	};
-  const hErc721Transfer = async (input: TxnInput) => {
+	const hErc721Transfer = async (input: TxnInput) => {
 		lg('hErc721Transfer() ... input:', input);
 		hErc721Transfer_loading = true;
 		const out = await erc721Transfer(input);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      hErc721Transfer_loading = false;
+			hErc721Transfer_loading = false;
 			return true;
 		}
 		t = getToastOk('');
@@ -205,66 +223,66 @@
 	};
 	const handleEthBalance = async (input: TxnInput) => {
 		lg('handleEthBalance() ... input:', input);
-    ethBalance_loading = true;
-    const out = await getBalanceEth(input.addr1);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		ethBalance_loading = true;
+		const out = await getBalanceEth(input.addr1);
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      ethBalance_loading = false;
+			ethBalance_loading = false;
 			return true;
 		}
-		t = getToastOk('balance: '+ out.str);
+		t = getToastOk('balance: ' + out.str);
 		toastStore.trigger(t);
-    ethBalance = out.str;
-    ethBalance_loading = false;
+		ethBalance = out.str;
+		ethBalance_loading = false;
 	};
 	const hErc20BalanceOf = async (input: TxnInput) => {
 		lg('hErc20BalanceOf() ... input:', input);
-    hErc20BalanceOf_loading = true;
-    const out = await erc20BalanceOf(input);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		hErc20BalanceOf_loading = true;
+		const out = await erc20BalanceOf(input);
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      hErc20BalanceOf_loading = false;
+			hErc20BalanceOf_loading = false;
 			return true;
 		}
-		t = getToastOk('balance: '+ out.str);
+		t = getToastOk('balance: ' + out.str);
 		toastStore.trigger(t);
-    tokenBalance = out.str;
-    lg('tokenBalance:', tokenBalance);
-    hErc20BalanceOf_loading = false;
+		tokenBalance = out.str;
+		lg('tokenBalance:', tokenBalance);
+		hErc20BalanceOf_loading = false;
 	};
 	const hErc721BalanceOf = async (input: TxnInput) => {
 		lg('hErc721BalanceOf() ... input:', input);
-    hErc721BalanceOf_loading = true;
-    const out = await erc721BalanceOf(input);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		hErc721BalanceOf_loading = true;
+		const out = await erc721BalanceOf(input);
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      hErc721BalanceOf_loading = false;
+			hErc721BalanceOf_loading = false;
 			return true;
 		}
-		t = getToastOk('balance: '+ out.str);
+		t = getToastOk('balance: ' + out.str);
 		toastStore.trigger(t);
-    nftBalance = out.str;
-    lg('nftBalance:', nftBalance);
-    hErc721BalanceOf_loading = false;
+		nftBalance = out.str;
+		lg('nftBalance:', nftBalance);
+		hErc721BalanceOf_loading = false;
 	};
 	const hErc20Allowance = async (input: TxnInput) => {
 		lg('hErc20Allowance() ... input:', input);
-    tokenAllowance_loading = true;
-    const out = await erc20Allowance(input);
-    if (out.err) {
-      t = getToastErr(JSON.stringify(out.err));
+		tokenAllowance_loading = true;
+		const out = await erc20Allowance(input);
+		if (out.err) {
+			t = getToastErr(JSON.stringify(out.err));
 			toastStore.trigger(t);
-      tokenAllowance_loading = false;
+			tokenAllowance_loading = false;
 			return true;
 		}
-		t = getToastOk('allowance: '+ out.str);
+		t = getToastOk('allowance: ' + out.str);
 		toastStore.trigger(t);
-    tokenAllowance = out.str;
-    lg('tokenAllowance:', tokenAllowance);
-    tokenAllowance_loading = false;
+		tokenAllowance = out.str;
+		lg('tokenAllowance:', tokenAllowance);
+		tokenAllowance_loading = false;
 	};
 
 	const sendPost = async () => {
@@ -331,17 +349,17 @@
 		</select>
 	</label>
 
-  <label class="label">
+	<label class="label">
 		<span>Contract</span>
 		<select class="select" bind:value={ctrtName}>
 			<option value="goldcoin">ERC20 Gold Coin</option>
 			<option value="erc721Dragon">ERC721 Dragon NFT</option>
 		</select>
-    <span>Contract Addr:</span>
-    <input class="input" title="Contract Addr" type="text" bind:value={input.ctrtAddr} />
+		<span>Contract Addr:</span>
+		<input class="input" title="Contract Addr" type="text" bind:value={input.ctrtAddr} />
 	</label>
 
-  <label class="label">
+	<label class="label">
 		<span>Address1</span>
 		<input
 			class="input"
@@ -382,8 +400,10 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-primary" on:click={() => handleEthBalance(input)}
-				>Get ETH Balance</button
+			<button
+				type="button"
+				class="btn variant-filled-primary"
+				on:click={() => handleEthBalance(input)}>Get ETH Balance</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
@@ -400,8 +420,10 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-primary" on:click={() => hErc20BalanceOf(input)}
-				>Get Token Balance</button
+			<button
+				type="button"
+				class="btn variant-filled-primary"
+				on:click={() => hErc20BalanceOf(input)}>Get Token Balance</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
@@ -419,8 +441,10 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-primary" on:click={() => hErc20Allowance(input)}
-				>Get Token Allowance</button
+			<button
+				type="button"
+				class="btn variant-filled-primary"
+				on:click={() => hErc20Allowance(input)}>Get Token Allowance</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
@@ -438,8 +462,10 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-secondary" on:click={() => getNativeBalcM()}
-				>Get Native Balance</button
+			<button
+				type="button"
+				class="btn variant-filled-secondary"
+				on:click={() => console.log('clicked for getNativeBalcM()')}>Get Native Balance</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
@@ -457,8 +483,10 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-secondary" on:click={() => getTokbalcsM()}
-				>Get Token Balances</button
+			<button
+				type="button"
+				class="btn variant-filled-secondary"
+				on:click={() => console.log('clicked for getTokbalcsM()')}>Get Token Balances</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
@@ -476,8 +504,10 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-secondary" on:click={() => getNftmetadataM()}
-				>Get NFT Metadata</button
+			<button
+				type="button"
+				class="btn variant-filled-secondary"
+				on:click={() => console.log('clicked for getNftmetadataM()')}>Get NFT Metadata</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
@@ -499,7 +529,7 @@
 				>Transfer ERC20 Tokens</button
 			>
 		{/if}
-    <div class="mt-2 ml-2">
+		<div class="mt-2 ml-2">
 			Txn Hash: {txnHash_transfer20}
 		</div>
 	</div>
@@ -518,12 +548,12 @@
 				>Approve ERC20 Allowance</button
 			>
 		{/if}
-    <div class="mt-2 ml-2">
+		<div class="mt-2 ml-2">
 			Txn Hash: {txnHash_approve20}
 		</div>
 	</div>
 
-  <div class="container">
+	<div class="container">
 		<p>
 			Receipt:<br />
 		</p>
@@ -531,7 +561,7 @@
 		</pre>
 	</div>
 
-  <div class="flex flex-row">
+	<div class="flex flex-row">
 		{#if hErc721BalanceOf_loading}
 			<ProgressRadial
 				...
@@ -541,15 +571,17 @@
 				width="w-14"
 			/>
 		{:else}
-			<button type="button" class="btn variant-filled-primary" on:click={() => hErc721BalanceOf(input)}
-				>Get NFT Balance</button
+			<button
+				type="button"
+				class="btn variant-filled-primary"
+				on:click={() => hErc721BalanceOf(input)}>Get NFT Balance</button
 			>
 		{/if}
 		<div class="mt-2 ml-2">
 			NFT Balance: {nftBalance}
 		</div>
 	</div>
-  <div class="flex flex-row">
+	<div class="flex flex-row">
 		{#if hErc721Transfer_loading}
 			<ProgressRadial
 				...
@@ -563,7 +595,7 @@
 				>Transfer 1 ERC721 NFT</button
 			>
 		{/if}
-    <div class="mt-2 ml-2">
+		<div class="mt-2 ml-2">
 			Txn Hash: {txnHash_transfer721}
 		</div>
 	</div>
